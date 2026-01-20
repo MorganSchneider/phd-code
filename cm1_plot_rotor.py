3a36c13c19e1a7b125bdbfded8dab952aab75884
MorganSchneider <me.schneider0108@gmail.com>
2026-01-20 14:55:35 -0500

    Update code for Matt

--- cm1_plot_rotor.py
+++ cm1_plot_rotor.py
@@ -39,28 +39,15 @@ fp = 'C:/Users/mschne28/Documents/stuff_for_matt/'
 plottime = 220 #Time to plot model fields (min) -- anywhere from probably 215-220 to look at the rotor development
 
 
-#%% Load data
+#%% Load parcel data
 
 fnum = plottime - 167 # CM1 output file number
-mvtime = 220
 
 
 # ds = nc.Dataset(fp + f"cm1out_{fnum:06d}.nc")
-# time = ds.variables['time'][:].data[0]
 # xh = ds.variables['xh'][:].data
 # yh = ds.variables['yh'][:].data
 # zh = ds.variables['z'][:].data
-
-# xvort = ds.variables['xvort'][:].data[0,:,:,:]
-# yvort = ds.variables['yvort'][:].data[0,:,:,:]
-# hvort = np.sqrt(xvort**2 + yvort**2)
-
-# thrpert = ds.variables['thrpert'][:].data[:,:,:]
-
-# uinterp = ds.variables['uinterp'][:].data[0,:,:,:]
-# vinterp = ds.variables['vinterp'][:].data[0,:,:,:]
-# winterp = ds.variables['winterp'][:].data[0,:,:,:]
-
 # ds.close()
 
 
@@ -73,20 +60,18 @@ dbfile.close()
 
 
 
-
-
 dbfile = open(fp + "traj_MV1.pkl", 'rb')
 traj = pickle.load(dbfile)
-xp = traj[f"{mvtime}min"]['x']
-yp = traj[f"{mvtime}min"]['y']
-zp = traj[f"{mvtime}min"]['z']
-up = traj[f"{mvtime}min"]['u']
-vp = traj[f"{mvtime}min"]['v']
-wp = traj[f"{mvtime}min"]['w']
+xp = traj["220min"]['x']
+yp = traj["220min"]['y']
+zp = traj["220min"]['z']
+up = traj["220min"]['u']
+vp = traj["220min"]['v']
+wp = traj["220min"]['w']
 dbfile.close()
 
 
-dbfile = open(fp + f"traj_clusters_{mvtime}min_v2.pkl", 'rb')
+dbfile = open(fp + "traj_clusters_220min_v2.pkl", 'rb')
 dat = pickle.load(dbfile)
 cc = dat['mv1']
 x_ml = xp[:,(cc==1)]
@@ -108,10 +93,10 @@ dbfile.close()
 mtime = np.linspace(10800, 14400, 61) # CM1 output times
 ptime = np.linspace(10800, 14400, 241) # Parcel output times
 
-ti = np.where(ptime == mvtime*60)[0][0]
+ti = np.where(ptime == 220*60)[0][0]
 
 
-# Interpolate storm motion from model output timesto parcel output times (60 s to 15 s)
+# Interpolate storm motion from model output times to parcel output times (60 s to 15 s)
 fu = interp1d(mtime, u_storm)
 u_storm_prcl = fu(ptime)
 fv = interp1d(mtime, v_storm)
@@ -133,16 +118,13 @@ for i in inds:
     y_sr[i,:] = y_ml[i] + delta_y
 
 
-
-
 # Load saved parcel trajectory horizontal vorticity (interpolated from model fields)
-dbfile = open(fp + f"hvort_traj_{mvtime}min.pkl", 'rb')
+dbfile = open(fp + "hvort_traj_220min.pkl", 'rb')
 vort_traj = pickle.load(dbfile)
 xvort_ml = vort_traj['xvort_ml']
 yvort_ml = vort_traj['yvort_ml']
 hvort_ml = np.sqrt(xvort_ml**2 + yvort_ml**2)
 dbfile.close()
-
 
 
 # Median parcel trajectory values
@@ -157,14 +139,16 @@ yvort_median = np.median(yvort_ml[:ti+1,:], axis=1)
 hvort_median = np.median(hvort_ml[:ti+1,:], axis=1)
 
 
+
 #%% Make plots to investigate rotor -> model output cross sections
 
 ti = np.where(ptime == plottime*60)[0][0]
 
-
+# x/y indices of median parcel trajectory position at t=plottime
 ix = np.argmin(np.abs(xh - x_median[ti]))
 iy = np.argmin(np.abs(yh - y_median[ti]))
 
+# Load data
 ds = nc.Dataset(fp + f"cm1out_{fnum:06d}.nc")
 # X-Z cross section
 thrpert_xz = ds.variables['thrpert'][:].data[:,iy,:]
@@ -186,9 +170,13 @@ hvort_yz = np.sqrt(xvort_yz**2 + yvort_yz**2)
 ds.close()
 
 
-levs = np.linspace(0, 0.02, 21) #contour levels for hvort
+levs = np.linspace(0, 0.02, 21) #contour levels for hvort - change if you plot a different variable
 
-# Plot cross sections
+# plot limits - change as you want
+xlim = [-15, 0]
+ylim = [-60, -45]
+zlim = [0, 3]
+
 
 # X-Z cross section -- Horizontal vorticity magnitude
 fig,ax = plt.subplots(1, 1, figsize=(8,6), subplot_kw=dict(box_aspect=1), layout='constrained')
@@ -196,8 +184,8 @@ c = ax.contourf(xh, zh, hvort_xz, levels=levs, vmin=levs[0], vmax=levs[-1], cmap
 c.set_edgecolor('face')
 ax.quiver(xh[::4], zh[::3], u_xz[::3,::4]+6, w_xz[::3,::4], color='k', scale=500, width=0.003, pivot='tail')
 ax.scatter(x_median[:ti+1], z_median[:ti+1], s=30, c='k') #parcel median SR trajectory
-ax.set_xlim(-15, 0)
-ax.set_ylim(0, 3)
+ax.set_xlim(xlim)
+ax.set_ylim(zlim)
 ax.set_title(f"\u03c9$_H$ X-Z cross section, {plottime} min", fontsize=14)
 ax.set_xlabel('x (km)', fontsize=12)
 ax.set_ylabel('z (km)', fontsize=12)
@@ -215,8 +203,8 @@ c = ax.contourf(yh, zh, hvort_yz, levels=levs, vmin=levs[0], vmax=levs[-1], cmap
 c.set_edgecolor('face')
 # ax.quiver(yh[::4], zh[::3], v_yz[::3,::4], w_yz[::3,::4], color='k', scale=500, width=0.003, pivot='tail')
 ax.scatter(y_median[:ti+1], z_median[:ti+1], s=30, c='k') #parcel median SR trajectory
-ax.set_xlim(-60, -45)
-ax.set_ylim(0, 3)
+ax.set_xlim(ylim)
+ax.set_ylim(zlim)
 ax.set_title(f"\u03c9$_H$ Y-Z cross section, {plottime} min", fontsize=14)
 ax.set_xlabel('y (km)', fontsize=12)
 ax.set_ylabel('z (km)', fontsize=12)
@@ -237,7 +225,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 ti = np.where(ptime == plottime*60)[0][0]
 
-
+# x/y/z indices of median parcel trajectory position at t=plottime
 ix = np.argmin(np.abs(xh - x_median[ti]))
 iy = np.argmin(np.abs(yh - y_median[ti]))
 iz = np.argmin(np.abs(zh - z_median[ti]))
@@ -270,7 +258,9 @@ hvort_yz = np.sqrt(xvort_yz**2 + yvort_yz**2)
 ds.close()
 
 
+levs = np.linspace(0, 0.02, 21) #contour levels for hvort - change if you plot a different variable
 
+# plot limits - change as you want
 xlim = [-15, 0]
 ylim = [-60, -45]
 zlim = [0, 3]
